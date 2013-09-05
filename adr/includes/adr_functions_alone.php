1aaf7f8ec61cac8dec8963db2c0f3247b1c8c436
Nami-Doc <vendethiel@hotmail.fr>
2013-09-05 19:05:13 +0200

    I swear these people do the ugliest indent

--- adr/includes/adr_functions_alone.php
+++ adr/includes/adr_functions_alone.php
@@ -269,112 +269,113 @@ function adr_display_poster_infos($poster_id, $character_info, $race_info, $elem
 	$character_rank_info 	= character_rank_info();
 	$total_ranks			= number_format(count($character_rank_info));
 
-	if((!$topic_config[0]) && (!$topic_config[1]) && (!$topic_config[2]) && (!$topic_config[3]) && (!$topic_config[4]) && (!$topic_config[5]))
+	if((!$topic_config[0]) && (!$topic_config[1]) && (!$topic_config[2]) && (!$topic_config[3]) && (!$topic_config[4]) && (!$topic_config[5])) {
 		return $adr_topic_box;
+	}
 
-		for ($adr = 0; $adr < count($character_info); $adr++)
+	for ($adr = 0; $adr < count($character_info); $adr++)
+	{
+		$adr_topic_box = '';
+		if (($character_info[$adr]['character_id'] == $poster_id) && ($character_info[$adr]['character_class'] > '0'))
+		{
+			for ($x = 0; $x < count($character_rank_info); $x++)
 			{
-			$adr_topic_box = '';
-			if (($character_info[$adr]['character_id'] == $poster_id) && ($character_info[$adr]['character_class'] > '0'))
-				{
-				for ($x = 0; $x < count($character_rank_info); $x++)
-					{
-					if ($character_rank_info[$x]['character_id'] == $poster_id)
-						$rank = sprintf($lang['adr_stats_rank'], ($x + 1), $total_ranks);
-					}
+				if ($character_rank_info[$x]['character_id'] == $poster_id)
+					$rank = sprintf($lang['adr_stats_rank'], ($x + 1), $total_ranks);
+			}
 
-				$adr_topic_box .= '<fieldset class="fieldset" style="margin: 0px 0px 0px 0px;">';
-				$adr_topic_box .= '<legend>'. $lang['Adr_character'] .'</legend>';
+			$adr_topic_box .= '<fieldset class="fieldset" style="margin: 0px 0px 0px 0px;">';
+			$adr_topic_box .= '<legend>'. $lang['Adr_character'] .'</legend>';
 
-				#==== Show INACTIVE or JAILED status, if necessary
+			#==== Show INACTIVE or JAILED status, if necessary
 //				if($character_info[$adr]['character_prefs_active'] == '0'){
 //					$adr_topic_box .= ($poster_id == $userdata['user_id']) ? sprintf($lang['Adr_character_inactive_topic'], '<a href="'. append_sid('adr_character_prefs.'. $phpEx .').'">', '</a>').'<br><br>' : sprintf($lang['Adr_character_inactive_topic'], '<b>', '</b>').'<br><br>';
 //				}
 //				elseif(($character_info[$adr]['character_prefs_active'] != '0') && ($user_cell_time > '0')){
-				if($user_cell_time > '0'){
-					$adr_topic_box .= ($poster_id != $userdata['user_id']) ? sprintf($lang['Adr_character_status_jail_topic'], '<a target="_blank" href="'. append_sid('adr_courthouse.'. $phpEx .'?celled_user_id='.$poster_id).'">', '</a>').'<br><br>' : sprintf($lang['Adr_character_status_jail_topic'], '<b>', '</b>').'<br><br>';
-				}
+			if($user_cell_time > '0'){
+				$adr_topic_box .= ($poster_id != $userdata['user_id']) ? sprintf($lang['Adr_character_status_jail_topic'], '<a target="_blank" href="'. append_sid('adr_courthouse.'. $phpEx .'?celled_user_id='.$poster_id).'">', '</a>').'<br><br>' : sprintf($lang['Adr_character_status_jail_topic'], '<b>', '</b>').'<br><br>';
+			}
 
-				#==== Show character_name
-				if ($topic_config[5])
-					$adr_topic_box .= '&nbsp;<a href="'. append_sid('adr_character.'. $phpEx .'?u='. $character_info[$adr]['character_id']) .'">'. $character_info[$adr]['character_name'] .'</a><br>';
+			#==== Show character_name
+			if ($topic_config[5])
+				$adr_topic_box .= '&nbsp;<a href="'. append_sid('adr_character.'. $phpEx .'?u='. $character_info[$adr]['character_id']) .'">'. $character_info[$adr]['character_name'] .'</a><br>';
 
-				#==== Show current rank
-				if ($topic_config[7])
-				$adr_topic_box .= '&nbsp;&nbsp;'. $rank .'<br>';
+			#==== Show current rank
+			if ($topic_config[7])
+			$adr_topic_box .= '&nbsp;&nbsp;'. $rank .'<br>';
 
-				##=== Show character level
-				if ($topic_config[0])
-					$adr_topic_box .= '&nbsp;&nbsp;'. $lang['Adr_topics_level'] .':&nbsp;'. $character_info[$adr]['character_level'] .'<br />';
+			##=== Show character level
+			if ($topic_config[0])
+				$adr_topic_box .= '&nbsp;&nbsp;'. $lang['Adr_topics_level'] .':&nbsp;'. $character_info[$adr]['character_level'] .'<br />';
 
-				#==== Show PvP link where applicable
+			#==== Show PvP link where applicable
 //				if(($topic_config[6]) && ($character_info[$adr]['prefs_pvp_allow'] == '1') && ($character_info[$adr]['character_prefs_active'] == '1') && ($character_info[$adr]['character_id'] != '0') && ($userdata['user_id'] > '0'))
-				if(($topic_config[6]) && ($character_info[$adr]['prefs_pvp_allow'] == '1') && ($character_info[$adr]['character_id'] != '0') && ($userdata['user_id'] > '0'))
+			if(($topic_config[6]) && ($character_info[$adr]['prefs_pvp_allow'] == '1') && ($character_info[$adr]['character_id'] != '0') && ($userdata['user_id'] > '0'))
+			{
+				#==== Check if the user hasn't already exceeded his maximum number of defies allowed
+				$total_battles = 0;
+				for ($pvp = 0; $pvp < count($pvp_info); $pvp++)
 				{
-					#==== Check if the user hasn't already exceeded his maximum number of defies allowed
-					$total_battles = 0;
-					for ($pvp = 0; $pvp < count($pvp_info); $pvp++)
+					if ( ($pvp_info[$pvp]['battle_opponent_id'] == $poster_id || $pvp_info[$pvp]['battle_challenger_id'] == $poster_id) && ($pvp_info[$pvp]['battle_result'] == '0' || $pvp_info[$pvp]['battle_result'] == '3') )
 					{
-						if ( ($pvp_info[$pvp]['battle_opponent_id'] == $poster_id || $pvp_info[$pvp]['battle_challenger_id'] == $poster_id) && ($pvp_info[$pvp]['battle_result'] == '0' || $pvp_info[$pvp]['battle_result'] == '3') )
-						{
-							$total_battles = $total_battles + 1;
+						$total_battles = $total_battles + 1;
 						if (!$pvp_info[$pvp]['battle_opponent_id'])
 							break;
-						}
 					}
+				}
 
-					if ( ($adr_general['battle_pvp_defies_max'] >= $total_battles) && $adr_general['battle_pvp_defies_max'] && $poster_id != $userdata['user_id'] )
+				if ( ($adr_general['battle_pvp_defies_max'] >= $total_battles) && $adr_general['battle_pvp_defies_max'] && $poster_id != $userdata['user_id'] )
+				{
+					for ($pvp = 0; $pvp < count($pvp_info); $pvp++)
 					{
-						for ($pvp = 0; $pvp < count($pvp_info); $pvp++)
+						if ( ($pvp_info[$pvp]['battle_challenger_id'] == $poster_id || $pvp_info[$pvp]['battle_challenger_id'] == $userdata['user_id']) && ($pvp_info[$pvp]['battle_opponent_id'] == $poster_id || $pvp_info[$pvp]['battle_opponent_id'] == $userdata['user_id']) && ($pvp_info[$pvp]['battle_result'] == '0' || $pvp_info[$pvp]['battle_result'] == '3') )
 						{
-							if ( ($pvp_info[$pvp]['battle_challenger_id'] == $poster_id || $pvp_info[$pvp]['battle_challenger_id'] == $userdata['user_id']) && ($pvp_info[$pvp]['battle_opponent_id'] == $poster_id || $pvp_info[$pvp]['battle_opponent_id'] == $userdata['user_id']) && ($pvp_info[$pvp]['battle_result'] == '0' || $pvp_info[$pvp]['battle_result'] == '3') )
-							{
-								$this_battle_id		= $pvp_info[$pvp]['battle_id'];
-								$this_battle_turn 	= $pvp_info[$pvp]['battle_turn'];
-								$this_battle_result = $pvp_info[$pvp]['battle_result'];
-								break;
-							}
-						} #==== Close 2nd for array
+							$this_battle_id		= $pvp_info[$pvp]['battle_id'];
+							$this_battle_turn 	= $pvp_info[$pvp]['battle_turn'];
+							$this_battle_result = $pvp_info[$pvp]['battle_result'];
+							break;
+						}
+					} #==== Close 2nd for array
 
-						if ($this_battle_id)
+					if ($this_battle_id)
+					{
+						#==== Check if is current chars turn
+						if($this_battle_turn == $userdata['user_id'])
 						{
-							#==== Check if is current chars turn
-							if($this_battle_turn == $userdata['user_id'])
-							{
-								$adr_topic_box .= '&nbsp;&nbsp;'. $lang['Adr_pvp_post_text'] . ':&nbsp;<a href="'. append_sid("adr_battle_pvp.$phpEx?battle_id=". $this_battle_id) .'" target="_parent">'. $lang['Adr_pvp_post_your_turn'] .'</a><br />';
-							}
-							elseif(($this_battle_turn == 0) && ($this_battle_result == 0))
-							{
-								$opponent_turn = sprintf($lang['Adr_pvp_post_opponent_turn'], $character_info[$adr]['character_name']);
-								$adr_topic_box .= '&nbsp;&nbsp;'. $lang['Adr_pvp_post_text'] . ':&nbsp;<a href="'. append_sid("adr_character_pvp.$phpEx?mode=waiting") .'" target="_parent"><i>'. $lang['Adr_pvp_opponent_awaiting'] .'</i></a><br />';
-							}
-							else
-							{
-								$opponent_turn = sprintf($lang['Adr_pvp_post_opponent_turn'], $character_info[$adr]['character_name']);
-								$adr_topic_box .= '&nbsp;&nbsp;'. $lang['Adr_pvp_post_text'] . ':&nbsp;<a href="'. append_sid("adr_battle_pvp.$phpEx?mode=see&amp;battle_id=". $this_battle_id) .'" target="_parent">'. $opponent_turn .'</a><br />';
-							}
+							$adr_topic_box .= '&nbsp;&nbsp;'. $lang['Adr_pvp_post_text'] . ':&nbsp;<a href="'. append_sid("adr_battle_pvp.$phpEx?battle_id=". $this_battle_id) .'" target="_parent">'. $lang['Adr_pvp_post_your_turn'] .'</a><br />';
+						}
+						elseif(($this_battle_turn == 0) && ($this_battle_result == 0))
+						{
+							$opponent_turn = sprintf($lang['Adr_pvp_post_opponent_turn'], $character_info[$adr]['character_name']);
+							$adr_topic_box .= '&nbsp;&nbsp;'. $lang['Adr_pvp_post_text'] . ':&nbsp;<a href="'. append_sid("adr_character_pvp.$phpEx?mode=waiting") .'" target="_parent"><i>'. $lang['Adr_pvp_opponent_awaiting'] .'</i></a><br />';
 						}
 						else
 						{
-							$total_battles = 0;
-							for ($pvp = 0; $pvp < count($pvp_info); $pvp++)
+							$opponent_turn = sprintf($lang['Adr_pvp_post_opponent_turn'], $character_info[$adr]['character_name']);
+							$adr_topic_box .= '&nbsp;&nbsp;'. $lang['Adr_pvp_post_text'] . ':&nbsp;<a href="'. append_sid("adr_battle_pvp.$phpEx?mode=see&amp;battle_id=". $this_battle_id) .'" target="_parent">'. $opponent_turn .'</a><br />';
+						}
+					}
+					else
+					{
+						$total_battles = 0;
+						for ($pvp = 0; $pvp < count($pvp_info); $pvp++)
+						{
+							if ( ($pvp_info[$pvp]['battle_opponent_id'] == $userdata['user_id'] || $pvp_info[$pvp]['battle_challenger_id'] == $userdata['user_id']) && ($pvp_info[$pvp]['battle_result'] == '0' || $pvp_info[$pvp]['battle_result'] == '3') )
 							{
-								if ( ($pvp_info[$pvp]['battle_opponent_id'] == $userdata['user_id'] || $pvp_info[$pvp]['battle_challenger_id'] == $userdata['user_id']) && ($pvp_info[$pvp]['battle_result'] == '0' || $pvp_info[$pvp]['battle_result'] == '3') )
-								{
-									$total_battles = $total_battles + 1;
-									if (!$pvp_info[$pvp]['battle_opponent_id'])
-										break;
-								}
+								$total_battles = $total_battles + 1;
+								if (!$pvp_info[$pvp]['battle_opponent_id'])
+									break;
 							}
-							$total = $total_battles;
+						}
+						$total = $total_battles;
 
-							if ( ($adr_general['battle_pvp_defies_max'] >= $total) && ($adr_general['battle_pvp_defies_max']) && ($poster_id != $userdata['user_id']))
-								$adr_topic_box .= '&nbsp;&nbsp;'. $lang['Adr_pvp_post_text'] . ':&nbsp;<a href="'. append_sid("adr_character_pvp.$phpEx?mode=defy_action&amp;defied=". $poster_id).'" target="_parent">'. $lang['Adr_pvp_post_attack'] .'</a><br />';
-							}
-						} #==== Close 2nd if statement
-					} #==== End of pvp check
+						if ( ($adr_general['battle_pvp_defies_max'] >= $total) && ($adr_general['battle_pvp_defies_max']) && ($poster_id != $userdata['user_id']))
+							$adr_topic_box .= '&nbsp;&nbsp;'. $lang['Adr_pvp_post_text'] . ':&nbsp;<a href="'. append_sid("adr_character_pvp.$phpEx?mode=defy_action&amp;defied=". $poster_id).'" target="_parent">'. $lang['Adr_pvp_post_attack'] .'</a><br />';
+						}
+					} #==== Close 2nd if statement
+				} #==== End of pvp check
 
-				if ($topic_config[8]){
+			if ($topic_config[8]){
 				$adr_topic_box .= '<br>'.$lang['Adr_character_battle_stats_title'].':<br>';
 				$adr_topic_box .= '&nbsp;&nbsp;'.$lang['Adr_monster_list_hp'].': '.$character_info[$adr]['character_hp'].'/'.$character_info[$adr]['character_hp_max'].'<br>';
 				$adr_topic_box .= '&nbsp;&nbsp;'.$lang['Adr_monster_list_mp'].': '.$character_info[$adr]['character_mp'].'/'.$character_info[$adr]['character_mp_max'].'<br>';
@@ -382,102 +383,102 @@ function adr_display_poster_infos($poster_id, $character_info, $race_info, $elem
 				$adr_topic_box .= '&nbsp;&nbsp;'.$lang['Adr_monster_list_def'].':&nbsp;'. adr_battle_make_def($character_info[$adr]['character_ac'], $character_info[$adr]['character_dexterity']) .'<br>';
 				$adr_topic_box .= '&nbsp;&nbsp;'.$lang['Adr_monster_list_ma'].':&nbsp;'. adr_battle_make_magic_att($character_info[$adr]['character_intelligence']) .'<br>';
 				$adr_topic_box .= '&nbsp;&nbsp;'.$lang['Adr_monster_list_md'].':&nbsp;'. adr_battle_make_magic_def($character_info[$adr]['character_wisdom']) .'<br>';
-					}
+			}
 
-				if (($topic_config[1]) || ($topic_config[2]) || ($topic_config[3]) || ($topic_config[4]))
-					$adr_topic_box .= '<br>'.$lang['Adr_character_characteristics'].':<br>';
+			if (($topic_config[1]) || ($topic_config[2]) || ($topic_config[3]) || ($topic_config[4]))
+				$adr_topic_box .= '<br>'.$lang['Adr_character_characteristics'].':<br>';
 
-				if ($topic_config[1])
-					{
+			if ($topic_config[1])
+			{
 				$adr_topic_box .= '&nbsp;&nbsp;'. $lang['Adr_topics_class'].':&nbsp;&nbsp;';
 
-					for ($class = 0; $class < count($class_info); $class++)
-						{
-						if ($character_info[$adr]['character_class'] == $class_info[$class]['class_id'])
-							{
+				for ($class = 0; $class < count($class_info); $class++)
+				{
+					if ($character_info[$adr]['character_class'] == $class_info[$class]['class_id'])
+					{
 						$class_lang = adr_get_lang($class_info[$class]['class_name']);
 						$class_img	= $class_info[$class]['class_img'];
 						break;
-							}
-						}
+					}
+				}
 
-					if ($topic_config[1] == '1')
-						$adr_topic_box .= $class_lang;
-					else
-						$adr_topic_box .= '<img src="adr/images/classes/'. $class_img .'">';
+				if ($topic_config[1] == '1')
+					$adr_topic_box .= $class_lang;
+				else
+					$adr_topic_box .= '<img src="adr/images/classes/'. $class_img .'">';
 
 				$adr_topic_box .= '<br />';
-					} #==== Close topic_config[1] if statement
+			} #==== Close topic_config[1] if statement
 
-				if ($topic_config[2])
+			if ($topic_config[2])
+			{
+				$adr_topic_box .= '&nbsp;&nbsp;'. $lang['Adr_topics_race'].':&nbsp;&nbsp;';
+
+				for ($race = 0; $race < count($race_info); $race++)
+				{
+					if ($character_info[$adr]['character_race'] == $race_info[$race]['race_id'])
 					{
-					$adr_topic_box .= '&nbsp;&nbsp;'. $lang['Adr_topics_race'].':&nbsp;&nbsp;';
+						$race_lang 	= adr_get_lang($race_info[$race]['race_name']);
+						$race_img	= $race_info[$race]['race_img'];
+						break;
+					}
+				}
 
-					for ($race = 0; $race < count($race_info); $race++)
-						{
-						if ($character_info[$adr]['character_race'] == $race_info[$race]['race_id'])
-							{
-								$race_lang 	= adr_get_lang($race_info[$race]['race_name']);
-								$race_img	= $race_info[$race]['race_img'];
-								break;
-							}
-						}
-
-					if ($topic_config[2] == '1')
-						$adr_topic_box .= $race_lang;
-					else
-						$adr_topic_box .= '<img src="adr/images/races/'. $race_img .'">';
+				if ($topic_config[2] == '1')
+					$adr_topic_box .= $race_lang;
+				else
+					$adr_topic_box .= '<img src="adr/images/races/'. $race_img .'">';
 				$adr_topic_box .= '<br />';
-					} #==== Close topic_config[2] if statement
+			} #==== Close topic_config[2] if statement
 
-				if ($topic_config[3])
-					{
+			if ($topic_config[3])
+			{
 				$adr_topic_box .= '&nbsp;&nbsp;'. $lang['Adr_topics_element'].':&nbsp;&nbsp;';
 
-					for ($elements = 0; $elements < count($element_info); $elements++)
-						{
-						if ($character_info[$adr]['character_element'] == $element_info[$elements]['element_id'])
-							{
+				for ($elements = 0; $elements < count($element_info); $elements++)
+				{
+					if ($character_info[$adr]['character_element'] == $element_info[$elements]['element_id'])
+					{
 						$element_lang 	= adr_get_lang([$element_info[$elements]['element_name']]);
 						$element_img	= $element_info[$elements]['element_img'];
 						break;
-							}
-						}
+					}
+				}
 
-					if ($topic_config[3] == '1')
-						$adr_topic_box .= $element_lang;
-					else
-						$adr_topic_box .= '<img src="adr/images/elements/'. $element_img .'">';
+				if ($topic_config[3] == '1')
+					$adr_topic_box .= $element_lang;
+				else
+					$adr_topic_box .= '<img src="adr/images/elements/'. $element_img .'">';
 				$adr_topic_box .= '<br />';
-					} #==== Close topic_config[3] if statement
+			} #==== Close topic_config[3] if statement
 
-				if ($topic_config[4])
+			if ($topic_config[4])
+			{
+				$adr_topic_box .= '&nbsp;&nbsp;'. $lang['Adr_topics_alignment'].':&nbsp;&nbsp;';
+
+				for ($alignment = 0; $alignment < count($alignment_info); $alignment++)
+				{
+					if ($character_info[$adr]['character_alignment'] == $alignment_info[$alignment]['alignment_id'])
 					{
-					$adr_topic_box .= '&nbsp;&nbsp;'. $lang['Adr_topics_alignment'].':&nbsp;&nbsp;';
-
-					for ($alignment = 0; $alignment < count($alignment_info); $alignment++)
-						{
-						if ($character_info[$adr]['character_alignment'] == $alignment_info[$alignment]['alignment_id'])
-							{
 						$alignment_lang = adr_get_lang([$alignment_info[$alignment]['alignment_name']]);
 						$alignment_img = $alignment_info[$alignment]['alignment_img'];
 						break;
-							}
-						}
+					}
+				}
 
-					if ($topic_config[4] == '1')
-						$adr_topic_box .= $alignment_lang;
-					else
-						$adr_topic_box .= '<img src="adr/images/alignments/'. $alignment_img .'">';
+				if ($topic_config[4] == '1')
+					$adr_topic_box .= $alignment_lang;
+				else
+					$adr_topic_box .= '<img src="adr/images/alignments/'. $alignment_img .'">';
 				$adr_topic_box .= '<br />';
-					} #==== Close topic_config[4] if statement
+			} #==== Close topic_config[4] if statement
 
-				$adr_topic_box .= '</fieldset>';
-				break;
-				} #==== End if statement for matching id
-			} #==== End for array
-		return $adr_topic_box;
-		} #==== End function
+			$adr_topic_box .= '</fieldset>';
+			break;
+		} #==== End if statement for matching id
+	} #==== End for array
+	return $adr_topic_box;
+} #==== End function
 
 #====
 #==== Author: aUsTiN [austin@phpbb-amod.com] [http://phpbb-tweaks.com] = |
@@ -513,5 +514,3 @@ function adr_ban_check($user_id)
 		adr_previous ( Adr_character_ban , 'index' , '' );
 	}
 }
-	
-?>
